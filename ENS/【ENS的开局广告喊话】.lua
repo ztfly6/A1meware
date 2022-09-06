8d7bf4be30b8342764f3eed961f9b3c8bc70bda2
ztfly6 <99885238+ztfly6@users.noreply.github.com>
2022-09-06 10:03:34 +0800

    Delete ENS directory

--- ENS/【ENS的开局广告喊话】.lua
+++
@@ -1,9 +0,0 @@
-﻿local roundtext ={"我正在使用A1mware的大神ENS参数 不要迷恋A1mware参数哥 扫码+2329554378"}
-
-local function roundsay(e)
-if e:GetName() == "round_start" then
-client.ChatSay( tostring( roundtext[1] ) );
-end
-end
-callbacks.Register( "FireGameEvent", roundsay );
-client.AllowListener( 'round_start' );
